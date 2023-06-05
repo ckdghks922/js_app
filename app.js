e64a35fb1d6af141e6062018c31a4775ad01a178
changhwan park <57472727+ckdghks922@users.noreply.github.com>
2023-06-05 17:50:41 +0900

    feat: 초기화, 지우개 기능 추가

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const ctx = canvas.getContext("2d");
 
 const inputRange = document.getElementById('canvas-width');
 const inputColor = document.getElementById('canvas-color');
+const inputClear = document.getElementById('canvas-clear');
+const inputEraser = document.getElementById('canvas-eraser');
 
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_SIZE = 800;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 ctx.lineWidth = inputRange.value;
 
 let isDrawing = false;
+let isErase = false;
 
 const handleDraw = () => {
   isDrawing = true;
@@ -35,7 +40,25 @@ const handleWidth = event => {
 const handleColor = event => {
   ctx.strokeStyle = event.target.value;
   ctx.fillStyle = event.target.value;
+  if (isErase) handleEraser();
+};
+const handleClear = () => {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+};
+const handleEraser = () => {
+  if (isErase) {
+    isErase = false;
+    ctx.strokeStyle = inputColor.value;
+    inputEraser.value = '지우개 모드';
+  } else {
+    isErase = true;
+    ctx.strokeStyle = 'white';
+    inputEraser.value = '그리기 모드';
+  }
 };
 
 inputRange.addEventListener('change', handleWidth);
 inputColor.addEventListener('change', handleColor);
+inputClear.addEventListener('click', handleClear);
+inputEraser.addEventListener('click', handleEraser);
